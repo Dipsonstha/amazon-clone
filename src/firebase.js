c29e8b1e31f8829c8c20c383c47b9cf76b476737
Dipsonstha <dipson9861@gmail.com>
2024-01-01 22:59:50 +0545

    removed unwanted files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAcCwAObdbxUd8vdiX9pD4XtWuJcN-gbSs",
+    authDomain: "challenge-8ffea.firebaseapp.com",
+    projectId: "challenge-8ffea",
+    storageBucket: "challenge-8ffea.appspot.com",
+    messagingSenderId: "222138701111",
+    appId: "1:222138701111:web:939896faa21c0f4032f6b2",
+    measurementId: "G-0LKE82E4T2"
+  };
